27d415c84c942d1ce3a9cbc3f3635cd265ae7a1f
Julien Renaux <julien.renaux@gmail.com>
2012-04-11 22:39:45 +0800

    update alert + scaffolding for AlertRequest

--- js/mercor-alert.js
+++ js/mercor-alert.js
@@ -51,30 +51,23 @@ var MercorAlert = new Class({
 		'node': {
 			'id': 'mercor-alert',
 			'width' : 300,
-			'delay': 5000,
+			'duration': 5000,
 			'opacity' : 0.9,
 			'template':	'<div class="mercor-inner">'
 				+'<div class="mercor-close" title="Close"></div>'
 				+'<div class="mercor-title">{TITLE}</div>'
 				+'<div class="mercor-text">{TEXT}</div>'
 			+'</div>'
-		}
+		},
+		'fade': {
+			'duration': 250,
+		    'transition': 'linear'
+		},
+		onOpen: null,
+		onClose: null,
+		onFadeIn: null,
+		onFadeOut: null
 	},
-
-	/**
-	 * The node element
-	 */
-	node: null,
-	
-	/**
-	 * The Close button element
-	 */
-	buttonClose: null,
-
-	/**
-	 * Store the node event
-	 */
-	event: null,
 
 	initialize: function(options){
 		// set the options
@@ -93,43 +86,6 @@ var MercorAlert = new Class({
 			'class': this.options.container.position
 		}).inject(this.options.container.el || document.body,'bottom');
 	},
-	
-	_addEvents: function()
-	{
-		var o = this;
-		// Add the delete event
-		this.buttonClose.addEvent('click',function(event){
-			o.close();
-			event.stop();
-		});
-		
-		this.event = new Fx.Tween(this.node,{
-			// Duration divided by 3 because 3 actions => Start, Hold, Close
-			duration: this.options.node.delay / 3,
-			link: 'chain',
-			transition: Fx.Transitions.Sine.easeOut,
-			onChainComplete : function(){
-				this.close();
-			}.bind(this)
-		});
-
-		// Event Start: Fade In
-		this.event.start('opacity',0,this.options.node.opacity);
-		// Event Hold
-		this.event.start('opacity',this.options.node.opacity,this.options.node.opacity);
-		// Event Close: Fade Out
-		this.event.start('opacity',this.options.node.opacity,0);
-
-		// Add the Pause event when hovering the container
-		this.container.addEvents({
-			mouseenter: function(){
-				this.event.pause();
-		    }.bind(this),
-		    mouseleave: function(){
-		    	this.event.resume();
-		    }.bind(this)
-		});
-	},
 
 	_injectNode: function(options){
 		var title = (options.title || '').toString();
@@ -142,20 +98,101 @@ var MercorAlert = new Class({
 		this.node.setStyle('opacity', 0);
 		this.node.inject(this.container, 'top');
 	},
+	
+	_setupNode: function(){
+		this.buttonClose = this.node.getElement('.mercor-close');
+		var close = function(){
+			this.close();
+		}.bind(this);
+		// Add the delete event
+		this.buttonClose.addEvent('click',function(event){
+			close();
+			event.stop();
+		});
+		this.fade = new Fx.Morph(this.node, {
+			duration: this.options.fade.duration,
+			transition: this.options.fade.transition
+		});
+		close.delay(this.options.node.duration);
+	},
+	
+	_fadeIn: function(){
+		this.fade.start({'opacity': [0, 1]});
+		this.fireEvent('fadeIn');
+	},
+	
+	_fadeOut: function(){
+		this.fade.start({'opacity': [1,0]});
+		this.fireEvent('fadeOut');
+	},
 
 	open: function(options){
-		// Set the container position
 		this.container.set('class',this.options.container.position);
-		// Inject the node
 		this._injectNode(options);
-		// Get the button close element
-		this.buttonClose = this.node.getElement('.mercor-close');
-		// Add events
-		this._addEvents();
+		this._setupNode();
+		this._fadeIn();
+		this.fireEvent('open');
 	},
 
 	close: function(){
-		if(!this.node) return;
-		this.node.destroy();
+		if (this.fade){
+			this.fade.addEvent('onChainComplete',function(){
+				if(!this.node) return;
+				this.node.destroy();
+			}.bind(this));
+			this._fadeOut();
+		}
+		this.fireEvent('close');
 	}
+});
+
+MercorAlert.Request = new Class({
+	
+	Extends: MercorAlert,
+
+	Implements : [Events, Options],
+	
+	options:{
+		'request': {
+			'url': '',
+			'method': 'get',
+			'asynch': true,
+			'data': '',
+			'success': function(responseText, body){body.set('text', responseText);}
+	    }
+	},
+	
+	initialize: function(options){
+		this.parent(options);
+	},
+	
+	_load: function() {
+		this.header.set('html',this.options.title);
+		var requestOptions = {
+			url : this.options.request.url,
+			data : this.options.request.data,
+			async : this.options.request.async,
+			method : this.options.request.method,
+			onRequest: function(){
+				this._loadStart();
+				this.fireEvent('request');
+			}.bind(this),
+			onSuccess: function(responseText){
+				this.options.request.success(responseText).inject(this.content);
+				this.fireEvent('success');
+			}.bind(this),
+			onFailure: function() {
+				this._failure();
+				this.fireEvent('failure');
+			}.bind(this),
+			onComplete: function() {
+				this.content.inject(this.body);
+				this._loadStop();
+				this.fireEvent('complete');
+			}.bind(this)
+		};
+		this.request = new Request(requestOptions);
+		this.request.send();
+	}
+
 });
